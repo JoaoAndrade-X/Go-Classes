3d69cb792b5cf28a823f06205d65e19c8ea97cdc
Joao <joaogandrade@hotmail.com>
2026-02-20 22:43:45 -0300

    Working with strings

---
+++ 7section/4-regex/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+)
+
+func main() {
+	text1 := "Hello world! Welcome to Go"
+
+	regGo, err := regexp.Compile("Go")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Text '%s', matches 'Go': %t\n", text1, regGo.MatchString(text1))
+
+	text2 := "Products codes: P123, x342, P789"
+
+	rProductP := regexp.MustCompile(`P\d+`)
+	firstProduct := rProductP.FindString(text2)
+	fmt.Println(firstProduct)
+
+	allPProducts := rProductP.FindAllString(text2, -1)
+	fmt.Println(allPProducts)
+}
